2c6120f64df14441d25d0ab6b3b2c96d699b6c79
Adam Brodin <worecraft@gmail.com>
2021-03-17 14:38:01 +0100

    Finished check name match from StarWars API

--- Source/SpacePark/Networking/StarWarsAPI.cs
+++ Source/SpacePark/Networking/StarWarsAPI.cs
@@ -1,15 +1,13 @@
-﻿using RestSharp;
-using System;
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SpacePark.Networking
 {
-    class APIResponse
-    {
-
-    }
     public class StarWarsAPI
     {
-        readonly string apiUrl = "https://swapi.dev/api/";
+        readonly string apiUrl = "https://swapi.dev/api";
         RestClient client;
 
         public StarWarsAPI()
@@ -19,13 +17,19 @@ namespace SpacePark.Networking
 
         public bool UserFromStarWars(string name)
         {
-            RestRequest request = new RestRequest("people", Method.GET);
+            RestRequest request = new RestRequest("/people/", Method.GET);
             request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
 
             var result = client.Execute(request);
+            var jsonObject = JObject.Parse(result.Content);
 
-            Console.WriteLine(result.Content);
-            return false;
+            List<string> names = new List<string>();
+            foreach(var entry in jsonObject["results"])
+            {
+                names.Add(entry["name"].ToString());
+            }
+
+            return names.Any(n => n.ToLower() == name.ToLower());
         }
     }
 }
