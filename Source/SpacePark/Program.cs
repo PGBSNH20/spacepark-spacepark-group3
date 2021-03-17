69c46288636a4abff7385ad747edf074dcb02612
Adam Brodin <worecraft@gmail.com>
2021-03-17 13:32:08 +0100

    VS Project setup

---
+++ Source/SpacePark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpacePark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
