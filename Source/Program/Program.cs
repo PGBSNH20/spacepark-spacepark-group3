078afa225e7394be6c2b6ebd7814faeb8f7f93a3
Aswan1 <aswanalaeady@gmail.com>
2021-03-18 23:52:20 +0100

    added user calss added context class storde data to data base working

--- Source/Program/Program.cs
+++ Source/Program/Program.cs
@@ -9,6 +9,21 @@ namespace Program
         {
             Class1 c = new Class1();
             Console.WriteLine(c.Test());
+
+            using (var ctx = new Context())
+            {
+                var user = new User() {
+                    Id = 1,
+                    Name = "Luke Skywalker",                    
+                    ArrivalTime = DateTime.Now,
+                    DepartureTime = DateTime.Now.AddHours(5),
+                    PaymentAmount = 100,
+                
+                };
+
+                ctx.Users.Add(user);
+                ctx.SaveChanges();
+            }
         }
     }
 }
---
+++ Source/SpacePark/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace SpacePark
+{
+    public class Context : DbContext
+    {
+        public Context() : base()
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\\SQLEXPRESS, 1433;Initial Catalog=SpacePark;Trusted_Connection=True;");
+        }
+
+    }
+}
