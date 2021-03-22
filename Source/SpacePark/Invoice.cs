7658e4405e0290e02a1d9e24c878ea154d4159f6
Kadar Abbas <kadar-abbas@hotmail.com>
2021-03-22 23:16:41 +0100

    Add Invoice Class

---
+++ Source/SpacePark/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePark
+{
+    class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public DateTime RegistrationTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double Price { get; set; }
+        
+
+    }
+}
