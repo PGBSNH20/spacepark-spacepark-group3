38694b320e3f28276abcb87397874269e75270cb
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-03-24 19:56:46 +0100

    Added basic spots

--- Source/SpacePark/DB/DbContext.cs
+++ Source/SpacePark/DB/DbContext.cs
@@ -22,5 +22,19 @@ namespace SpacePark.DB
             optionsBuilder.UseNpgsql(this._connString);
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DBSpot>().HasData(
+                new DBSpot(1, 20, 120),
+                new DBSpot(2, 20, 120),
+                new DBSpot(3, 50, 280),
+                new DBSpot(4, 50, 280),
+                new DBSpot(5, 50, 280),
+                new DBSpot(6, 100, 600),
+                new DBSpot(7, 100, 600),
+                new DBSpot(8, 100, 1600),
+                new DBSpot(9, 1000, 8000)
+            );
+        }
     }
 }
--- Source/SpacePark/DB/Models/DBSpot.cs
+++ Source/SpacePark/DB/Models/DBSpot.cs
@@ -11,5 +11,12 @@ namespace SpacePark.DB.Models
         public int Size { get; set; }
         [Required]
         public decimal Price { get; set; }
+
+        public DBSpot(int ID, int size, decimal price)
+        {
+           this.ID = ID;
+           this.Size = size;
+           this.Price = price;
+        }
     }
 }
