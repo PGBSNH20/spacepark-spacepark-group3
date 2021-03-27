b4715294231d73028ad4ded73b02a97812306bc7
Kadar Abbas <kadar-abbas@hotmail.com>
2021-03-27 09:44:29 +0100

    Add Services file and print class

---
+++ Source/SpacePark/Services/InvoicePrint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacePark.Services
+{ 
+
+        public static void InvoicePrint(Invoice invoice, int i = 0)
+        {
+            if (i % 1 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            Console.WriteLine($"InvoiceID: {invoice.InvoiceID}\n " +
+            $"Ship: {invoice.ShipName}\n " +
+            $"StarteTime: {invoice.StarteTime}\n " +
+            $"EndTime: {invoice.EndTime}\n " +
+            $"TotalTime:{invoice.TotalTime}\n" +
+            $"HourlyPrice:{invoice.HourlyPrice}\n" +
+            $"TotalPrice:{invoice.TotalPrice}\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+
