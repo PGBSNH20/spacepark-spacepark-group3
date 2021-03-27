7cc91267266b63663cbf2fc2b0b12b95cd1701a4
Kadar Abbas <kadar-abbas@hotmail.com>
2021-03-27 09:36:41 +0100

    Revision Invoice class

--- Source/SpacePark/DB/Models/Invoice.cs
+++ Source/SpacePark/DB/Models/Invoice.cs
@@ -8,12 +8,14 @@ namespace SpacePark.Models
 {
     public class Invoice
     {
-            public DateTime StartedTime { get; set; }
+            public int InvoiceID { get; set; }
+            public string ShipName { get; set; }
+            public DateTime StarteTime { get; set; }
             public DateTime EndTime { get; set; }
             public double TotalTime { get; set; }
             public double HourlyPrice { get; set; }
             public double TotalPrice { get; set; }
-        }
-    }
+        } 
+}
 
 
